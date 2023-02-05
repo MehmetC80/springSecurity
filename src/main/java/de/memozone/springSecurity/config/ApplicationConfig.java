6d7972dcb3f68cce358cc1bca868f1daa9259db2
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-02-05 14:53:15 +0100

    update JwtAuthenticationFilter.java create ApplicationConfig.java

---
+++ src/main/java/de/memozone/springSecurity/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package de.memozone.springSecurity.config;
+
+import de.memozone.springSecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not Found !!!"));
+    }
+
+
+}
