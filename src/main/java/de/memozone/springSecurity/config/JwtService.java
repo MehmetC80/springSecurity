6c76f5a9f31b787bcc6aa5d4404f981b641ec2e1
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-02-05 14:00:59 +0100

    create JwtService.java update JwtAuthenticationFilter.java update pom.xml with jsonwebtoken

---
+++ src/main/java/de/memozone/springSecurity/config/JwtService.java
@@ -0,0 +1,12 @@
+package de.memozone.springSecurity.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String jwtToken) {
+
+
+        return null;
+    }
+}
